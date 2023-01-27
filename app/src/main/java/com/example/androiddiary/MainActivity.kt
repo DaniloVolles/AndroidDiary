f53a55c400a677e86eef41b62e84a6e2c1935210
Danilo Volles <danilo.volles@outlook.com>
2023-01-27 19:54:52 -0300

    Setting function of the floating action button on MainActivity.

--- app/src/main/java/com/example/androiddiary/MainActivity.kt
+++ app/src/main/java/com/example/androiddiary/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.androiddiary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.androiddiary.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addNoteBtn.setOnClickListener {
+            floatingActionButtonClick()
+        }
+    }
+
+    fun floatingActionButtonClick () {
+        val intent = Intent(this, DiaryEditorActivity::class.java)
+        startActivity(intent)
     }
 }
